fa085c9d58d05c5cea7c4ca4eeef838e293df6d8
user2 <polinaprotasova789@gmail.com>
2022-03-14 14:19:04 +0300

    laboratory four

---
+++ WpfApp4/WpfApp4/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp4
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+            using (AccountEntities db = new AccountEntities())
+            {
+                var login = new System.Data.SqlClient.SqlParameter
+                {
+                    ParameterName = "@login",
+                    SqlDbType = System.Data.SqlDbType.VarChar,
+                    Direction = System.Data.ParameterDirection.Input,
+                    Size = 50,
+                    Value = Login.Text
+                };
+                var password = new System.Data.SqlClient.SqlParameter
+                {
+                    ParameterName = "@password",
+                    SqlDbType = System.Data.SqlDbType.VarChar,
+                    Direction = System.Data.ParameterDirection.Input,
+                    Size = 50,
+                    Value = Password.Text
+                };
+                var result = new System.Data.SqlClient.SqlParameter
+                {
+                    ParameterName = "@result",
+                    SqlDbType = System.Data.SqlDbType.VarChar,
+                    Direction = System.Data.ParameterDirection.Output,
+                    Size = 250
+                };
+                db.Database.ExecuteSqlCommand("AddAccount @login, @password, @result OUTPUT", login, password, result);
+
+                db.SaveChanges();
+                MessageBox.Show("You are added");
+            }
+        }
+
+        private void RegBtn_Click(object sender, RoutedEventArgs e)
+        {
+            using (AccountEntities db = new AccountEntities())
+            {
+                var login = new System.Data.SqlClient.SqlParameter
+                {
+                    ParameterName = "@login",
+                    SqlDbType = System.Data.SqlDbType.VarChar,
+                    Direction = System.Data.ParameterDirection.Input,
+                    Size = 50,
+                    Value = Login.Text
+                };
+                var password = new System.Data.SqlClient.SqlParameter
+                {
+                    ParameterName = "@password",
+                    SqlDbType = System.Data.SqlDbType.VarChar,
+                    Direction = System.Data.ParameterDirection.Input,
+                    Size = 50,
+                    Value = Password.Text
+                };
+                var result = new System.Data.SqlClient.SqlParameter
+                {
+                    ParameterName = "@result",
+                    SqlDbType = System.Data.SqlDbType.VarChar,
+                    Direction = System.Data.ParameterDirection.Output,
+                    Size = 250
+                };
+                db.Database.ExecuteSqlCommand("UserLogin @login, @password, @result OUTPUT", login, password, result);
+
+                MessageBox.Show((string)result.Value);
+            }
+        }
+    }
+}
